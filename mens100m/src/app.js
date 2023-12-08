37597e42a9a05b3c385c011f2bd581d14112abc2
Saloni Gupta <70319586+saloni24gupta@users.noreply.github.com>
2023-12-08 11:25:25 +0530

    mongo express node connection

--- mens100m/src/app.js
+++ mens100m/src/app.js
@@ -1,7 +1,25 @@
 const express = require("express");
+require("../src/db/conn")
+const MensRanking = require("../src/models/mens")
 const app = express();
 
 const PORT = process.env.PORT || 3000;
+
+app.use(express.json())
+// we will handle post request
+app.post("/mens", async(req, res) => {
+    try{
+        const addingMensRecords = new MensRanking(req.body);
+console.log(req.body)
+        addingMensRecords.save();
+    }catch(e){
+      res.send(e);
+    }
+    
+  
+})
+
+
 app.get("/", async(req, res) => {
     res.send ("hello from me");
 })
