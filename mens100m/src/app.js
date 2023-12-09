3bbd7f319d649002e6d5800800a7c4f860e93238
Saloni Gupta <70319586+saloni24gupta@users.noreply.github.com>
2023-12-09 18:05:53 +0530

    apicompleted

--- mens100m/src/app.js
+++ mens100m/src/app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const router = require("../src/routers/men")
 require("../src/db/conn")
 const MensRanking = require("../src/models/mens")
 const app = express();
@@ -7,34 +8,8 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json())
 // we will handle post request
-app.post("/mens", async(req, res) => {
-    try{
-        const addingMensRecords = new MensRanking(req.body);
-console.log(req.body);
-const insertMens = await addingMensRecords.save();
-res.status(201).send(insertMens)  ;
 
-}catch(e){
-      res.status(400).send(e);
-    }
-    
-  
-})
-
-app.get("/mens", async(req, res) => {
-    try{
-
-        const getMens = await MensRanking.find({});
-       res.status(201).send(getMens)  ;
-
-}catch(e){
-      res.status(400).send(e);
-    }
-    
-  
-})
-
-
+app.use(router);
 app.listen(PORT,() => {
     console.log(`connection is live at port no ${PORT} `)
 })
