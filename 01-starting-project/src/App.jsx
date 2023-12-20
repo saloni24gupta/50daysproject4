79b2f70f7218e9a9b54a3590a954ab2dab28284f
Saloni Gupta <70319586+saloni24gupta@users.noreply.github.com>
2023-12-20 18:21:12 +0530

    variable

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -7,44 +7,64 @@ import TabButton from './components/TabButton.jsx';
 import { EXAMPLES } from './data.js';
 
 function App() {
-
+  let tabContent = 'Please click a button '
 
   function handleSelect(selectedButton) {
     // selectedButton => 'components', 'jsx', 'props', 'state'
-
-    console.log(selectedButton);
+    tabContent = selectedButton;
+    console.log(tabContent);
   }
-
-
-
-
+  console.log("app component execution");
   return (
     <div>
       <Header />
       <main>
         <section id="core-concepts">
           <h2>Core Concepts</h2>
-        
+<ul>
+  <CoreConcept 
+  tittle={CORE_CONCEPTS[0].title}
+  description={CORE_CONCEPTS[0].description}
+  image={CORE_CONCEPTS[0].image}
+  />
+
+<CoreConcept 
+  tittle={CORE_CONCEPTS[1].title}
+  description={CORE_CONCEPTS[1].description}
+  image={CORE_CONCEPTS[1].image}
+  />
+
+<CoreConcept 
+  tittle={CORE_CONCEPTS[2].title}
+  description={CORE_CONCEPTS[2].description}
+  image={CORE_CONCEPTS[2].image}
+  />
+    <CoreConcept 
+  tittle={CORE_CONCEPTS[3].title}
+  description={CORE_CONCEPTS[3].description}
+  image={CORE_CONCEPTS[3].image}
+  />
+</ul>
         </section>
         <section id="examples">
           <h2>Examples</h2>
           <menu>
             <TabButton onSelect={() => handleSelect('components')} >
               Components
-           
+
             </TabButton>
             <TabButton onSelect={() => handleSelect('JSX')} >
-            JSX
+              JSX
             </TabButton>
             <TabButton onSelect={() => handleSelect('pROPS')} >
-             Props
+              Props
             </TabButton>
             <TabButton onSelect={() => handleSelect('State')} >
               State
             </TabButton>
-           
+
           </menu>
-         dynamic conctent
+          {tabContent}
         </section>
       </main>
     </div>
