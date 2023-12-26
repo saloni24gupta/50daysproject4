6abc73ee34127b83c23925434b8d3e50977c52d5
Saloni Gupta <70319586+saloni24gupta@users.noreply.github.com>
2023-12-26 22:37:22 +0530

    continue

---
+++ 07-tic-tac-toe-starting-project/src/components/Player.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { useState } from "react"
+export default function Player({name, symbol}) {
+   
+    const [isEditing , setIsEditing] =  useState(false);
+    function handleEditClick() {
+        setIsEditing(true);
+    }
+    return (
+    <li>
+    <span className="player">
+      <span className="player-name">{name}</span>
+      <span className="player-symbol">{symbol}</span>
+    </span>
+    <button onClick={handleEditClick}>Edit</button>
+  </li>
+ 
+  )
+}
