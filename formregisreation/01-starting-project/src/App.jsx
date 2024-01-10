3c9894a5d08589237b6a58aaae3c6f6f50d446ab
Saloni Gupta <70319586+saloni24gupta@users.noreply.github.com>
2024-01-10 21:43:32 +0530

    Newsapi

--- formregisreation/01-starting-project/src/App.jsx
+++ formregisreation/01-starting-project/src/App.jsx
@@ -1,10 +1,10 @@
-
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <>
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum, consequuntur.
-    </>
+    <NavBar />
+      </>
   );
 }
 
