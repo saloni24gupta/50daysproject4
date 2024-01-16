ef3d7788e8b8777197fe78015999a552b8db64a7
Saloni Gupta <70319586+saloni24gupta@users.noreply.github.com>
2024-01-16 22:45:45 +0530

    commit

--- formregisreation/01-starting-project/src/App.jsx
+++ formregisreation/01-starting-project/src/App.jsx
@@ -1,9 +1,11 @@
 import NavBar from "./components/NavBar";
-
+import News from "./components/News";
+// 02ceb65115d64a8190ca3567b3773afa
 function App() {
   return (
     <>
     <NavBar />
+    <News />
       </>
   );
 }
--- formregisreation/01-starting-project/src/components/NavBar.jsx
+++ formregisreation/01-starting-project/src/components/NavBar.jsx
@@ -6,7 +6,7 @@ export default class NavBar extends Component {
       <div>
         <nav className="navbar navbar-expand-lg bg-body-tertiary">
   <div className="container-fluid">
-    <a className="navbar-brand" href="#">Navbar</a>
+    <a className="navbar-brand" href="#">News</a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
@@ -16,17 +16,17 @@ export default class NavBar extends Component {
           <a className="nav-link active" aria-current="page" href="#">Home</a>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="#">Link</a>
+          <a className="nav-link" href="/about">About</a>
         </li>
         <li className="nav-item dropdown">
           <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
             Dropdown
           </a>
           <ul className="dropdown-menu">
-            <li><a className="dropdown-item" href="#">Action</a></li>
-            <li><a className="dropdown-item" href="#">Another action</a></li>
+            <li><a className="dropdown-item" href="/">Action</a></li>
+            <li><a className="dropdown-item" href="/">Another action</a></li>
             <li><hr className="dropdown-divider"/></li>
-            <li><a className="dropdown-item" href="#">Something else here</a></li>
+            <li><a className="dropdown-item" href="/">Something else here</a></li>
           </ul>
         </li>
         <li className="nav-item">
