c0a08e444297069b4361ef42806469097017d3a9
Saloni Gupta <70319586+saloni24gupta@users.noreply.github.com>
2024-02-01 23:07:40 +0530

    setbackgrounimg

---
+++ 01-starting-project/src/components/BackgroundImage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import background from '../assets/login.jpg'
+import styled from 'styled-components'
+function BackgroundImage() {
+  return (
+    <Container>
+      <img src={background} alt="" />
+    </Container>
+  )
+}
+
+export default BackgroundImage
+const Container = styled.div``
